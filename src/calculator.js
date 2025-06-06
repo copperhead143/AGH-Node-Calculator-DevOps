9e24e35d111aec38043272dd4202a12951e9402e
copperhead143 <ssikora258@gmail.com>
2025-06-06 18:13:41 +0200

    fix and pr block

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,10 @@
 // src/calculator.js
 export function add(a, b) {
-    return a - b;
+    return a + b;
   }
   
   export function subtract(a, b) {
-    return a + b;
+    return a - b;
   }
   
   export function multiply(a, b) {
